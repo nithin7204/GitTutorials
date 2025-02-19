8f2892d9f14abffcdc5066d19effb5c010ba05d7
nithin7204 <nithinkumarkunchala@gmail.com>
2025-02-19 10:13:49 +0530

    this is first commit

---
+++ folderX/Solution.java
@@ -0,0 +1,7 @@
+class Solution{
+    public static void main(String[] args)
+    {
+        System.out.println("This is inside folder");
+        System.out.println("This is inside folder file");
+    }
+}
